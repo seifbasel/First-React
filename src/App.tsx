c2f614f6cb079b3a6216f4ba45a122f2cc576771
seif basel <seifbasel950@gmail.com>
2023-11-01 12:19:00 +0200

    enter

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import ListGroup from "./components/ListGroup";
+
+// import Message from "./massage";
+function App() {
+  return <div><ListGroup/></div>
+ 
+}
+export default App;
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,33 @@
+// import { useState } from "react";
+
+function ListGroup() {
+  return <h1>ahhjlaekfgjlase</h1>;
+  // let items = ["seif", "ahmed", "yousef"];
+  // // hook
+  // const [selected_index,set_selected_index]=useState(-1)
+
+  //   return (
+  //   <>
+  //     <h1 className="text-center">names</h1>
+  //     {items.length === 0 && <p>no item found</p>}
+  //     <ul className="list-group">
+  //       {items.map((item, index) => (
+  //         <li
+  //           className={
+  //             selected_index === index
+  //               ? "list-group-item active"
+  //               : "list-group-item"
+  //           }
+  //           key={item}
+  //           onClick={() => {
+  //             selected_index = index;
+  //           }}
+  //         >
+  //           {item}
+  //         </li>
+  //       ))}
+  //     </ul>
+  //   </>
+  // );
+}
+export default ListGroup;
